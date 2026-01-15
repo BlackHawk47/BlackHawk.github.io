b744cffd1d400789dc8df061fa592f89dda5385b
BlackHawk <danielzegwaard@gmail.com>
2026-01-15 13:14:17 +0100

    Project details pagina met subcomponenten

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from "./App";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import ProjectPage from "./pages/ProjectPage.jsX";
+import ProjectPage from "./pages/ProjectPage";
 
 import "./index.css";
 
